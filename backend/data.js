0c459f920a8b64a3570310140a83066fae5d33be
Djapegram <predrag.radojicic.17@singimail.rs>
2021-09-11 15:55:37 +0200

    first commit

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Dozer za sapun',
+            category:'Dozeri',
+            image:'/images/dozer.jpg',
+            price:30,
+            countInStock:5,
+            brand:'Plastiko',
+            rating:4.5,
+            numReviews: 10,
+            description:'Najkvelitenija plastika'
+        },
+        {
+            _id:'2',
+            name:'Dozer za sampon',
+            category:'Dozeri',
+            image:'/images/dozer2.jpg',
+            price:50,
+            countInStock:7,
+            brand:'Plastiko',
+            rating: 4.8,
+            numReviews: 17,
+            description:'Dozer za sampon'
+        },
+        {
+            _id:'3',
+            name:'Flasica za vodu',
+            category:'Flasa',
+            image:'/images/flasa.jpg',
+            price:10,
+            countInStock:20,
+            brand:'Plastiko',
+            rating:4.5,
+            numReviews: 10,
+            description:'Flasica spremna za brendiranje'
+        },
+        {
+            _id:'4',
+            name:'Flasica za trening',
+            category:'Flasa',
+            image:'/images/flasa1.jpg',
+            price:35,
+            countInStock:30,
+            brand:'Plastiko',
+            rating:4.5,
+            numReviews: 10,
+            description:'Kvalitetne flasice za trening'
+        },
+        {
+            _id:'5',
+            name:'Cinija za hranu',
+            category:'Cinija',
+            image:'/images/cinija2.jpg',
+            price:150,
+            countInStock:32,
+            brand:'Plastiko',
+            rating:4.5,
+            numReviews: 10,
+            description:'Cinija za rucak'
+        },
+        {
+            _id:'6',
+            name:'Kutija za rucak',
+            category:'Dozeri',
+            image:'/images/cinija.jpg',
+            price:200,
+            countInStock:15,
+            brand:'Plastiko',
+            rating:4.5,
+            numReviews: 10,
+            description:'Kutija za rucak'
+        },
+    ],
+  };
+  export default data;
